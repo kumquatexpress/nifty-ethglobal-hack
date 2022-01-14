b358ee203d2922f3eed3f75d2b77a4c01623f617
dxu <xudavidb@gmail.com>
2022-01-14 14:30:11 -0800

    rgba utils

--- client/src/scripts/utils/index.ts
+++ client/src/scripts/utils/index.ts
@@ -1,4 +1,9 @@
+import { RGBAColor } from "scripts/types";
 export function isMetaMaskInstalled() {
   // @ts-ignore
   return Boolean(ethereum && ethereum.isMetaMask);
+}
+
+export function RGBAColorToString(color: RGBAColor): string {
+  return `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`;
 }
