3e7b4d89f82ffcf53a950301968223f911c707b1
dxu <xudavidb@gmail.com>
2022-01-14 19:31:24 -0800

    update graphql client to support ws and uploading

--- client/src/graphql/GraphQLClient.ts
+++ client/src/graphql/GraphQLClient.ts
@@ -1,12 +1,25 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  from,
-  split,
-  HttpLink,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, from, split } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
+import { setContext } from "@apollo/client/link/context";
+import { createUploadLink } from "apollo-upload-client";
+import { WebSocketLink } from "@apollo/client/link/ws";
 import { getMainDefinition } from "@apollo/client/utilities";
+import { getLocalStorageKey } from "../utils/api_client";
+
+const wsLink = new WebSocketLink({
+  uri: `${process.env.REACT_APP_WS_PROTOCOL}${process.env.REACT_APP_SERV_HOSTNAME}/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      Authorization: localStorage.getItem(getLocalStorageKey()),
+    },
+  },
+});
+
+// TODO: fix this to not be hardcoded to localhost
+const uploadLink = createUploadLink({
+  uri: `${process.env.REACT_APP_SERV_PROTOCOL}${process.env.REACT_APP_SERV_HOSTNAME}/graphql`,
+});
 
 // TODO: only do this during development
 const errorLink = onError(({ graphQLErrors, networkError }) => {
@@ -20,13 +33,30 @@ const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (networkError) console.log(`[Network error]: ${networkError}`);
 });
 
-const splitLink = split(({ query }) => {
-  const definition = getMainDefinition(query);
-  return (
-    definition.kind === "OperationDefinition" &&
-    definition.operation === "subscription"
-  );
-}, from([errorLink, new HttpLink({ uri: "http://localhost:8080/graphql" })]));
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  // TODO: update this to proper auth
+  const token = localStorage.getItem(getLocalStorageKey());
+  // return the headers to the context so terminating link can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  from([errorLink, authLink, uploadLink])
+);
 
 const client = new ApolloClient({
   link: splitLink,
